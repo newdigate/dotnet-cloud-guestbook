0746a4ef5202401616f102e863930bf3b5bd65f6
Newdigate <nicnewdigate@gmail.com>
2022-01-05 19:25:34 +0000

    add aspnet promethius metrics exporter endpoint on port 4504

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -1,14 +1,44 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Prometheus;
 
 namespace dotnet_guestbook
 {
     public class Program
     {
+        private static readonly Counter TickTock =
+            Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
         public static void Main(string[] args)
         {
+            while (!Debugger.IsAttached) {
+                Thread.Sleep(1000);
+            }
+            var server = new MetricServer(hostname: "127.0.0.1", port: 1234);
+            server.RequestPredicate = request => 
+                {
+                    Console.WriteLine(request.Url.ToString());
+                    return true;
+                };
+            server.Start();
+
+            CancellationTokenSource cancelSource = new CancellationTokenSource();
+            Task background = Task.Factory.StartNew ( () => {
+                    while(!cancelSource.IsCancellationRequested) {
+                        Console.WriteLine("Tick/Tock");
+                        TickTock.Inc();
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+                }, cancelSource.Token);
+
             CreateHostBuilder(args).Build().Run();
+            cancelSource.Cancel();
+            background.Wait();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
