9f38ce5a3f82571ff9627270fb2cf579a3d0c244
Newdigate <nicnewdigate@gmail.com>
2022-01-15 09:15:27 +0000

    add metrics for get/post/error

--- src/frontend/Controllers/HomeController.cs
+++ src/frontend/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using frontend;
 using System.Net.Http.Headers;
+using Prometheus;
 
 namespace dotnet_guestbook.Controllers
 {
@@ -15,6 +16,10 @@ namespace dotnet_guestbook.Controllers
         private ILogger _logger;
         private IEnvironmentConfiguration _envConfig;
         private IHttpClientFactory _factory;
+        private static readonly Counter _metric_index_get = Metrics.CreateCounter("sampleapp_home_controller_index_get", "index_get");
+        private static readonly Counter _metric_index_get_error = Metrics.CreateCounter("sampleapp_home_controller_index_get_error", "index_get_error");
+        private static readonly Counter _metric_index_post = Metrics.CreateCounter("sampleapp_home_controller_index_post", "index_post");
+        private static readonly Counter _metric_index_post_error = Metrics.CreateCounter("sampleapp_home_controller_index_post_error", "index_post_error");
 
         public HomeController(
             IHttpClientFactory httpFactory,
@@ -30,7 +35,7 @@ namespace dotnet_guestbook.Controllers
         public async Task<IActionResult> Index()
         {
             _logger.LogInformation($"Getting all messages");
-
+            _metric_index_get.Inc();
             // Get the entries from the backend
             try
             {
@@ -48,6 +53,7 @@ namespace dotnet_guestbook.Controllers
             catch (Exception e)
             {
                 _logger.LogError(e.ToString());
+                _metric_index_get_error.Inc();
                 return View();
             }
         }
@@ -57,7 +63,7 @@ namespace dotnet_guestbook.Controllers
         public async Task<IActionResult> Post([FromForm] GuestbookEntry entry)
         {
             _logger.LogInformation($"Calling backend at {_envConfig.BackendAddress} for message authored by {entry.Name}");
-
+            _metric_index_post.Inc();
             try
             {
                 var httpClient = _factory.CreateClient();
@@ -70,6 +76,7 @@ namespace dotnet_guestbook.Controllers
             catch (Exception e)
             {
                 _logger.LogError(e.ToString());
+                _metric_index_post_error.Inc();
                 return View();
             }
         }
